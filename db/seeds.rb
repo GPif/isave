85f8128a5f13584f63324317483ffc199834b3b7
Guillaume GILLET <mailgpif@gmail.com>
2026-03-12 21:31:12 +0100

    build(seed): add seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+require 'json'
+
+json = JSON.parse(File.read('data/level_1/portfolios.json'))
+
+customer = Customer.create!(name: 'Client Principal', email: 'client@isave.fr')
+
+json['contracts'].each do |contract|
+  portfolio = customer.portfolios.create!(
+    label: contract['label'],
+    portfolio_type: Portfolio::PORTFOLIO_LOOKUP.invert[contract['type']],
+    amount: contract['amount']
+  )
+
+  next unless contract['lines']
+
+  contract['lines'].each do |line|
+    instrument = Instrument.find_or_create_by!(
+      isin: line['isin']
+    ) do |i|
+      i.label = line['label']
+      i.instrument_type = line['type']
+      i.price = line['price']
+      i.sri = line['srri']
+    end
+
+    portfolio.holdings.create!(
+      instrument: instrument,
+      amount: line['amount']
+    )
+  end
+end
